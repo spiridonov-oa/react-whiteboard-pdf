d189d750fb059f2c6f81ccb9e20ceefd47950f9c
Oleg Spiridonov <spiridonov.oa@gmail.com>
2023-06-04 13:08:45 -0400

    Update build

--- dist/components/ColorPicker/ColorPicker.styled.js
+++ dist/components/ColorPicker/ColorPicker.styled.js
@@ -11,7 +11,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _taggedTemplateLiteralLoose(strings, raw) { if (!raw) { raw = strings.slice(0); } strings.raw = raw; return strings; }
 
-var ColorPickerS = _styledComponents.default.div(_templateObject || (_templateObject = _taggedTemplateLiteralLoose(["\n  position: relative;\n  display: flex;\n  justify-content: center;\n  align-items: center;\n  border: 1px solid #999;\n  border-radius: 5px;\n  height: ", "px;\n  width: ", "px;\n  cursor: pointer;\n\n  > div {\n    box-shadow: 1px 1px 3px rgb(0 0 0 / 0.6);\n  }\n\n  &:hover {\n    background-color: #eee;\n  }\n"])), function (_ref) {
+var ColorPickerS = _styledComponents.default.div(_templateObject || (_templateObject = _taggedTemplateLiteralLoose(["\n  position: relative;\n  display: flex;\n  justify-content: center;\n  align-items: center;\n  border: 1px solid #999;\n  border-radius: 5px;\n  height: ", "px;\n  width: ", "px;\n  cursor: pointer;\n\n  > div {\n    //box-shadow: 1px 1px 3px rgb(0 0 0 / 0.6);\n  }\n\n  &:hover {\n    background-color: #eee;\n  }\n"])), function (_ref) {
   var size = _ref.size;
   return size || 34;
 }, function (_ref2) {
